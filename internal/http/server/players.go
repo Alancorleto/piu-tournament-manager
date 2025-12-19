174e860854a295acda54bbcf5cd6879c5b3938be
alancorleto <corletoalan@gmail.com>
2025-12-19 10:57:00 -0300

    Delete Player struct from the players handler

--- internal/http/server/players.go
+++ internal/http/server/players.go
@@ -3,25 +3,11 @@ package server
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/alancorleto/piu-tournament-manager/internal/http/codec/json"
 	"github.com/alancorleto/piu-tournament-manager/internal/http/dto"
 	"github.com/alancorleto/piu-tournament-manager/internal/http/mapper"
-	"github.com/google/uuid"
 )
-
-type Player struct {
-	ID                uuid.UUID `json:"id"`
-	Nickname          string    `json:"nickname"`
-	Name              *string   `json:"name"`
-	TeamName          *string   `json:"team_name"`
-	CountryCode       *string   `json:"country_code"`
-	City              *string   `json:"city"`
-	ProfilePictureUrl *string   `json:"profile_picture_url"`
-	CreatedAt         time.Time `json:"created_at"`
-	ModifiedAt        time.Time `json:"modified_at"`
-}
 
 func (s *Server) PostPlayersHandler(w http.ResponseWriter, r *http.Request) {
 	requestParams, err := json.ParseRequestParameters[dto.CreatePlayerRequest](r)
